c6fbec1dca37269d5e18781269c4dcf210e25d06
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2025-08-04 16:52:21 -0500

    Add files via upload

---
+++ backend-vet/src/routers/productoRoutes.js
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import {
+  crearProducto,
+  obtenerProductosPorEmprendedor,
+  obtenerProducto,
+  actualizarProducto,
+  eliminarProducto
+} from '../controllers/productoController.js';
+
+import { verificarTokenJWT } from '../middleware/JWT.js'; // Middleware de autenticación
+
+const router = Router();
+
+// Crear producto (solo con JWT)
+router.post('/', verificarTokenJWT, crearProducto);
+
+// Obtener todos los productos de un emprendedor
+router.get('/emprendedor/:emprendedorId', obtenerProductosPorEmprendedor);
+
+// Obtener un producto por ID
+router.get('/:id', obtenerProducto);
+
+// Actualizar producto (solo dueño)
+router.put('/:id', verificarTokenJWT, actualizarProducto);
+
+// Eliminar producto (solo dueño)
+router.delete('/:id', verificarTokenJWT, eliminarProducto);
+
+export default router;
