53999ef536fc364da67f35779c8db34101f487a3
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2025-08-05 16:59:32 -0500

    Update productoRoutes.js

--- backend-vet/src/routers/productoRoutes.js
+++ backend-vet/src/routers/productoRoutes.js
@@ -15,6 +15,8 @@ const router = Router();
 // Crear producto (solo con JWT)
 router.post('/', verificarTokenJWT, crearProducto);
 
+// Obtener todos los productos (públicos)
+router.get('/todos', obtenerTodosLosProductos);
 
 // Obtener todos los productos de un emprendedor
 router.get('/emprendedor/:emprendedorId', obtenerProductosPorEmprendedor);
@@ -27,7 +29,5 @@ router.put('/:id', verificarTokenJWT, actualizarProducto);
 
 // Eliminar producto (solo dueño)
 router.delete('/:id', verificarTokenJWT, eliminarProducto);
-// Obtener todos los productos (públicos)
-router.get('/todos', obtenerTodosLosProductos);
 
 export default router;
