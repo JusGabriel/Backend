15338f2d7bccca8d5289a89c39a4ffd749f7be05
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2025-08-05 20:12:53 -0500

    Update JWT.js

--- backend-vet/src/middleware/JWT.js
+++ backend-vet/src/middleware/JWT.js
@@ -19,18 +19,17 @@ const verificarTokenJWT = async (req, res, next) => {
         const { id, rol } = jwt.verify(token, process.env.JWT_SECRET)
 
         if (rol === "Administrador") {
-            // Aquí quitamos .lean() para obtener documento completo con _id ObjectId
-            req.adminBDD = await Administrador.findById(id).select("-password")
+            req.adminBDD = await Administrador.findById(id).lean().select("-password")
             if (!req.adminBDD) {
                 return res.status(404).json({ msg: "Administrador no encontrado" })
             }
         } else if (rol === "Emprendedor") {
-            req.emprendedorBDD = await Emprendedor.findById(id).select("-password")
+            req.emprendedorBDD = await Emprendedor.findById(id).lean().select("-password")
             if (!req.emprendedorBDD) {
                 return res.status(404).json({ msg: "Emprendedor no encontrado" })
             }
         } else if (rol === "Cliente") {
-            req.clienteBDD = await Cliente.findById(id).select("-password")
+            req.clienteBDD = await Cliente.findById(id).lean().select("-password")
             if (!req.clienteBDD) {
                 return res.status(404).json({ msg: "Cliente no encontrado" })
             }
