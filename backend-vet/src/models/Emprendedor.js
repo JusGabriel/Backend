e290cc9047719adec3e1352438c7400d242e5eb9
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2025-11-24 21:02:17 -0500

    Update Emprendedor.js

--- backend-vet/src/models/Emprendedor.js
+++ backend-vet/src/models/Emprendedor.js
@@ -20,74 +20,78 @@ const emprendedorSchema = new Schema({
   },
   password: {
     type: String,
-    required: function() {
-        // Requerido solo si NO hay Google OAuth
-        return !this.idGoogle;
+    required: function () {
+      return !this.idGoogle; // requerido solo si NO usa Google
     }
-    },
-    idGoogle: {
-      type: String,
-      default: null
-    },
+  },
+  idGoogle: {
+    type: String,
+    default: null
+  },
+
   descripcion: {
     type: String,
     trim: true,
     default: ''
   },
+
+  // Emprendimientos favoritos
   favoritos: [{
-  type: Schema.Types.ObjectId,
-  ref: 'Emprendimiento'
-}],
+    type: Schema.Types.ObjectId,
+    ref: 'Emprendimiento'
+  }],
+
   enlaces: {
     facebook: { type: String, default: null },
     instagram: { type: String, default: null },
     sitioWeb: { type: String, default: null }
   },
+
   telefono: {
     type: String,
     default: null,
     trim: true
   },
-  rol:{
-        type:String,
-        default:"Emprendedor"
-    },
-  
+
+  rol: {
+    type: String,
+    default: "Emprendedor"
+  },
+
   token: {
     type: String,
     default: null
   },
+
   confirmEmail: {
     type: Boolean,
     default: false
   },
+
   status: {
     type: Boolean,
     default: true
   },
+
   estado_Emprendedor: {
     type: String,
-    enum: ['Activo', 'Advertencia1','Advertencia2','Advertencia3', 'Suspendido'], 
+    enum: ['Activo', 'Advertencia1', 'Advertencia2', 'Advertencia3', 'Suspendido'],
     default: 'Activo'
   }
 }, {
   timestamps: true
 })
 
-/* -------------------- MÉTODOS -------------------- */
-
-// Cifrar contraseña
+/* ---------- MÉTODOS ---------- */
 emprendedorSchema.methods.encrypPassword = async function (password) {
   const salt = await bcrypt.genSalt(10)
   return await bcrypt.hash(password, salt)
 }
 
-// Comparar contraseñas
 emprendedorSchema.methods.matchPassword = async function (password) {
   return await bcrypt.compare(password, this.password)
 }
 
-// Generar token
 emprendedorSchema.methods.crearToken = function () {
   this.token = Math.random().toString(36).slice(2)
   return this.token
