e12e546a8a67fa17421779542a53d4c361656ec6
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2025-11-27 11:37:22 -0500

    Update Productos.js

--- backend-vet/src/models/Productos.js
+++ backend-vet/src/models/Productos.js
@@ -1,44 +1,44 @@
 import { Schema, model } from "mongoose";
 
 const productoSchema = new Schema({
-  nombre: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  descripcion: {
-    type: String,
-    trim: true
-  },
-  precio: {
-    type: Number,
-    required: true
-  },
-  imagen: {
-    type: String,
-    trim: true
-  },
-  emprendimiento: {
-    type: Schema.Types.ObjectId,
-    ref: "Emprendimiento",
-    required: true
-  },
-  categoria: {
-    type: Schema.Types.ObjectId,
-    ref: "Categoria",
-    default: null   // ✔ categoría opcional
-  },
-  stock: {
-    type: Number,
-    default: 0
-  },
-  estado: {
-    type: Boolean,
-    default: true
-  }
-},
-{
-  timestamps: true
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    descripcion: {
+        type: String,
+        trim: true
+    },
+    precio: {
+        type: Number,
+        required: true
+    },
+    imagen: {
+        type: String,
+        trim: true
+    },
+    emprendimiento: {
+        type: Schema.Types.ObjectId,
+        ref: 'Emprendimiento',
+        required: true
+    },
+    categoria: {
+        type: Schema.Types.ObjectId,
+        ref: 'Categoria',
+        required: false,   // <-- NO obligatoria
+        default: null      // <-- Puede venir vacía
+    },
+    stock: {
+        type: Number,
+        default: 0
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timestamps: true
 });
 
 export default model("Producto", productoSchema);
